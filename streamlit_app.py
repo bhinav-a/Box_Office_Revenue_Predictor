f2e99a1c1a5c1c5364cd2570ff51329fbc42820c
Abhinav Anurag <144872130+bhinav-a@users.noreply.github.com>
2025-01-22 23:41:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,23 @@ st.title(' 🤖 Machine Learning App')
 
 st.info('This app builds a Machine Learning Model')
 import pandas as pd
-
+df = pd.read_csv('boxoffice.csv')
+to_remove = ['world_revenue', 'opening_revenue']
+df.drop(to_remove, axis=1, inplace=True)
+df.drop('budget', axis=1, inplace=True)
+df.dropna(inplace=True)
 with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('boxoffice.csv')
   df
   st.write('**X**')
-  x = df.drop(['title','domestic_revenue'],axis=1 , inplace=True)
+  ab = ['title','domestic_revenue']
+  x = df.drop(ab,axis=1 , inplace=True)
   x
   st.write('**Y**')
   y = df['domestic_revenue']
   y
-to_remove = ['world_revenue', 'opening_revenue']
-df.drop(to_remove, axis=1, inplace=True)
-df.drop('budget', axis=1, inplace=True)
-df.dropna(inplace=True)
+
   
   
 
