cc0baf02cb9c2f0e53afaa68f1af8ec5886afd7f
Abhinav Anurag <144872130+bhinav-a@users.noreply.github.com>
2025-01-22 22:52:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,14 @@ st.title(' 🤖 Machine Learning App')
 
 st.info('This app builds a Machine Learning Model')
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import metrics
+from xgboost import XGBRegressor
 with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('boxoffice.csv')
