84352f81d0dc128aa933fb02f9d2d450a7bd86cf
Abhinav Anurag <144872130+bhinav-a@users.noreply.github.com>
2025-01-22 23:58:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ st.info('This app builds a Machine Learning Model')
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sb
 df = pd.read_csv('boxoffice.csv')
 to_remove = ['world_revenue', 'opening_revenue']
 df.drop(to_remove, axis=1, inplace=True)
