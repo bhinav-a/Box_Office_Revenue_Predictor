78ea369f5af5c0613d4b7fa4624e5c10c78b68a2
Abhinav Anurag <144872130+bhinav-a@users.noreply.github.com>
2025-01-23 00:42:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,10 @@ df.drop('genres', axis=1, inplace=True)
 features = df.drop(['title', 'domestic_revenue'], axis=1)
 target = df['domestic_revenue'].values
 
+for col in ['distributor', 'MPAA']:
+	le = LabelEncoder()
+	df[col] = le.fit_transform(df[col])
+
 X_train, X_val, Y_train, Y_val = train_test_split(x,y,test_size=0.2,random_state=22)
 
 scaler = StandardScaler()
