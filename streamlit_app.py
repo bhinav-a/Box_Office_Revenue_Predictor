cd35a10b3869170232da7924cef14adb84274252
Abhinav Anurag <144872130+bhinav-a@users.noreply.github.com>
2025-01-22 23:35:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,20 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import metrics
 from xgboost import XGBRegressor
-with st.expander('Data'):
-  st.write('**Raw Data**')
-  df = pd.read_csv('boxoffice.csv')
-  df
 to_remove = ['world_revenue', 'opening_revenue']
 df.drop(to_remove, axis=1, inplace=True)
 df.drop('budget', axis=1, inplace=True)
 df.dropna(inplace=True)
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('boxoffice.csv')
+  df
+  x = df.drop(['title','domestic_revenue'],axis=1 , inplace=True)
+  x
+  y = df['domestic_revenue']
+  y
+
+with st.expander('Data Visualization'):
+  
+  
 
