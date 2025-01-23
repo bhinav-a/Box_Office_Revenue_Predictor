5943c29c339a321568b1dee78a45008268f73e8e
Abhinav Anurag <144872130+bhinav-a@users.noreply.github.com>
2025-01-23 22:24:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,157 +1,90 @@
 import streamlit as st
-
-st.title('🎬Box Office Revenue Predictor')
-
-st.info('This app can predict a box office revenue by using the genre of the movie and other related features')
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sb
 import plotly.figure_factory as ff
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from xgboost import XGBRegressor
-@st.cache_data(ttl = TTl)
-df = pd.read_csv('boxoffice.csv')
-to_remove = ['world_revenue', 'opening_revenue']
-df.drop(to_remove, axis=1, inplace=True)
-df.drop('budget', axis=1, inplace=True)
-df.dropna(inplace=True)
+from sklearn.metrics import mean_absolute_error as mae
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv('boxoffice.csv')
+    df.drop(['world_revenue', 'opening_revenue', 'budget'], axis=1, inplace=True)
+    df.dropna(inplace=True)
+    for col in ['domestic_revenue', 'opening_theaters', 'release_days']:
+        df[col] = df[col].astype(str).str.replace(',', '')
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+        df[col] = df[col].apply(lambda x: np.log10(x))
+    for col in ['genres', 'distributor', 'MPAA']:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+    return df
+
+@st.cache_resource
+def train_model(features, target):
+    X_train, X_val, Y_train, Y_val = train_test_split(features, target, test_size=0.2, random_state=22)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_val = scaler.transform(X_val)
+    model = XGBRegressor()
+    model.fit(X_train, Y_train)
+    train_preds = model.predict(X_train)
+    val_preds = model.predict(X_val)
+    return model, scaler, mae(Y_train, train_preds), mae(Y_val, val_preds)
+
+df = load_data()
+x = df.drop(['title', 'domestic_revenue'], axis=1)
+y = df.domestic_revenue
+
+model, scaler, train_error, val_error = train_model(x, y)
+
+st.title('🎬Box Office Revenue Predictor')
+st.info('This app can predict a box office revenue by using the genre of the movie and other related features')
+
 with st.expander('Data'):
-  st.write('**Raw Data**')
-  df.head()
-  st.write('**X**')
-  ab = ['title','domestic_revenue']
-  x = df.drop(ab,axis=1)
-  x.head()
-  st.write('**Y**')
-  y = df.domestic_revenue
-  y.head()
-for col in ['domestic_revenue', 'opening_theaters', 'release_days']:
-    df[col] = df[col].astype(str).str.replace(',', '')
-    df[col] = pd.to_numeric(df[col], errors='coerce')
-features = ['domestic_revenue', 'opening_theaters', 'release_days']
-for col in features:
-  df[col] = df[col].apply(lambda x: np.log10(x))
+    st.write('**Raw Data**')
+    st.write(df.head())
+    st.write('**X**')
+    st.write(x.head())
+    st.write('**Y**')
+    st.write(y.head())
 
 group_labels = ['Domestic Revenue']
 array = df['domestic_revenue'].to_numpy()
-ar = [array]
-fig = ff.create_distplot(ar , group_labels ,bin_size=[.1, .25, .5])
+fig = ff.create_distplot([array], group_labels, bin_size=[.1, .25, .5])
 
 with st.expander('Data Visualization'):
-  st.plotly_chart(fig , use_container_width=True)
-
-for col in ['genres']:
-	le = LabelEncoder()
-	df[col] = le.fit_transform(df[col])
-
-for col in ['distributor', 'MPAA']:
-	le = LabelEncoder()
-	df[col] = le.fit_transform(df[col])
-
-ab = ['title','domestic_revenue' ]
-features = df.drop(ab, axis=1)
-target = df.domestic_revenue.values
-
-X_train, X_val, Y_train, Y_val = train_test_split(features,target,test_size=0.2,random_state=22)
-
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_val = scaler.transform(X_val)
-
-model = XGBRegressor()
-model.fit(X_train, Y_train)
-
-train_preds = model.predict(X_train)
-print('Training Error : ', mae(Y_train, train_preds))
-
-val_preds = model.predict(X_val)
-print('Validation Error : ', mae(Y_val, val_preds))
+    st.plotly_chart(fig, use_container_width=True)
 
 with st.sidebar:
-	load = st.button('Load Button')
-	if load:
-		st.header('Input Features')
-		title = st.text_input("Movie title", "Enter Movie Name")
-		pro = st.radio(
-		    "Choose the Production Company :",
-		    ['Warner Bros.', 'Disney', 'Sony', 'Universal', 'Paramount'],
-		)
-		
-		if pro == "Warner Bros.":
-		    dis = 4
-		elif pro == "Disney" :
-		    dis = 0
-		elif pro == "Sony" :
-		    dis = 2
-		elif pro == "Universal":
-		    dis = 3	
-		elif pro == "Paramount" :
-		    dis = 1
-	
-		mpaa = st.radio(
-		    "Choose MPAA :",
-		    ['R', 'G', 'NC', 'PG-13', 'PG'],
-		)
-		
-		if mpaa == 'R':
-		    mp = 4
-		elif mpaa == 'G' :
-		    mp = 0
-		elif mpaa == 'PG' :
-		    mp = 2
-		elif mpaa == 'PG-13':
-		    mp = 3	
-		elif mpaa == 'NC' :
-		    mp = 1
-	
-	
-		genre = st.radio(
-		    "Choose Genre:",
-		    ['Animation', 'Action', 'Horror', 'Comedy', 'Drama', 'Thriller'],
-		)
-		
-		if genre == "Animation":
-		    gen = 1
-		elif genre == "Action" :
-		    gen = 0
-		elif genre == "Horror" :
-		    gen = 4
-		elif genre == "Comedy":
-		    gen = 2	
-		elif genre == "Drama" :
-		    gen = 3
-		elif genre == "Thriller" :
-		    gen = 5
-	
-		open_T = st.slider('Opening Theatre' , 10 ,4500, 2263)  
-		release_D = st.slider('Release Date' , 1, 180 ,90)
-@st.cache_data(ttl=TTL)
-data = {
-	'title':title,
-	'distributor':pro,
-	'opening_theaters':open_T,
-	'MPAA' : mpaa, 
-	'genres' : genre,
-       'release_days': release_D
-	}
-input_df = pd.DataFrame(data,index=[0])
-data_en = {
-	'distributor':dis,
-	'opening_theaters':open_T,
-	'MPAA' : mp, 
-	'genres' : gen,
-       'release_days': release_D
-	}
-input_en = pd.DataFrame(data_en,index=[0])
-with st.expander("Input Data"):
-	st.write('**Data**')
-	input_df
-	st.write('**Encoded Data**')
-	input_en
-answer = model.predict(input_en.to_numpy())
-f_ans = 10**answer
-ans = f_ans[0]
-st.subheader('Predicted Rvenue')
-st.success('$ '+"{:,}".format(ans))
+    load = st.button('Load Button')
+    if load:
+        st.header('Input Features')
+        title = st.text_input("Movie title", "Enter Movie Name")
+        pro = st.radio("Choose the Production Company:", ['Warner Bros.', 'Disney', 'Sony', 'Universal', 'Paramount'])
+        mpaa = st.radio("Choose MPAA:", ['R', 'G', 'NC', 'PG-13', 'PG'])
+        genre = st.radio("Choose Genre:", ['Animation', 'Action', 'Horror', 'Comedy', 'Drama', 'Thriller'])
+        open_T = st.slider('Opening Theatre', 10, 4500, 2263)
+        release_D = st.slider('Release Date', 1, 180, 90)
+
+        pro_dict = {'Warner Bros.': 4, 'Disney': 0, 'Sony': 2, 'Universal': 3, 'Paramount': 1}
+        mpaa_dict = {'R': 4, 'G': 0, 'NC': 1, 'PG': 2, 'PG-13': 3}
+        genre_dict = {'Animation': 1, 'Action': 0, 'Horror': 4, 'Comedy': 2, 'Drama': 3, 'Thriller': 5}
+
+        data = {'title': title, 'distributor': pro, 'opening_theaters': open_T, 'MPAA': mpaa, 'genres': genre, 'release_days': release_D}
+        input_df = pd.DataFrame(data, index=[0])
+        data_en = {'distributor': pro_dict[pro], 'opening_theaters': open_T, 'MPAA': mpaa_dict[mpaa], 'genres': genre_dict[genre], 'release_days': release_D}
+        input_en = pd.DataFrame(data_en, index=[0])
+
+        with st.expander("Input Data"):
+            st.write('**Data**')
+            st.write(input_df)
+            st.write('**Encoded Data**')
+            st.write(input_en)
+
+        input_scaled = scaler.transform(input_en)
+        answer = model.predict(input_scaled)
+        ans = 10 ** answer[0]
+        st.subheader('Predicted Revenue')
+        st.success('$ ' + "{:,}".format(ans))
