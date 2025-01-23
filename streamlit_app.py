61bc79608f5b0564d85004178ad3b505fd48fc9b
Abhinav Anurag <144872130+bhinav-a@users.noreply.github.com>
2025-01-23 20:04:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,4 +153,4 @@ with st.expander("Input Data"):
 answer = model.predict(input_en.to_numpy())
 f_ans = str(10**answer)
 st.subheader('Predicted Rvenue')
-st.success('$'+f_ans)
+st.success('$'+f_ans[0])
